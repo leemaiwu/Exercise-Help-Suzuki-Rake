2b11ba486cd410f11f76563d4a9ba3d2325ebbd3
Laura Wu <leemaiwu@gmail.com>
2023-07-23 12:47:07 -0600

    Completed with map and replace method

--- solution.js
+++ solution.js
@@ -1,5 +1,6 @@
 const rakeGarden = (garden) => {
-  return garden
+  return garden.split(' ').map((x) => x === 'gravel' || x === 'rock' ? x : x.replace(x, 'gravel')).join(' ')
 }
 
+console.log(rakeGarden('slug spider rock gravel gravel gravel gravel gravel gravel gravel'))
 console.log(rakeGarden('slug spider rock gravel gravel gravel gravel gravel gravel gravel'))
